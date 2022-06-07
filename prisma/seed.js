a6d0e633957cd822345e2570ff793b788d05ae30
Toby Carlo Cripps <tobycarlocripps@jeannettes-imac.home>
2022-06-07 15:16:17 +0100

    added model Screen, Screening, & Movie

--- prisma/seed.js
+++ prisma/seed.js
@@ -17,11 +17,51 @@ async function seed() {
         }
     });
 
-
     console.log('Customer created', createdCustomer);
 
     // Add your code here
 
+    const createdContact = await prisma.contact.create({
+        data: {
+            phone: '123',
+            email: '123@example.com',
+            customer: {
+                create: {
+                    name: 'Bob',
+                },
+            },
+        },
+    });
+
+    const createdMovies = await prisma.movie.create ({
+        data: {
+            title: "Movie",
+            runtimeMins: 999
+        },
+    });
+
+    const createdScreen = await prisma.screen.create ({
+        data: {
+            number: 1,
+        },
+    });
+
+    const createdScreening = await prisma.screening.create ({
+        data: {
+            startsAt: new Date("2022-06-12T17:00"),
+            movie: {
+                create: {
+                    title: "Movie2",
+                    runtimeMins: 888
+                },
+            },
+            screen: {
+                create: {
+                    number: 2,
+                }
+            }
+        },
+    });
 
     // Don't edit any of the code below this line
     process.exit(0);
